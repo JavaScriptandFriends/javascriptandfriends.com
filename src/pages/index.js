64b7a1479216b8c245daf90c171d4824950d8a72
Baskar Rao <baskarrao.dandlamudi@outlook.com>
2019-01-14 16:32:43 -0500

    Updated index with code of conduct

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,19 @@ const Root = () => (
   <Layout hideHeading={true}>
     <div className="root">
       <h1>JavaScript and Friends Conference</h1>
-      <div className="infoRow">
+      <div className="infoRow">     
         <div>Columbus, Ohio</div>
         <div>August 2, 2019</div>
+      </div>
+      <div className="infoRow">
         <div>
           <Link to="vision">Vision Statement</Link>
         </div>
         <div>
           <Link to="callforspeakers">Call for Speakers</Link>
+        </div>
+        <div>
+          <Link to="codeofconduct">Code of Conduct</Link>
         </div>
       </div>
     </div>
