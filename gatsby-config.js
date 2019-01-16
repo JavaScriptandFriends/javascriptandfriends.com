bee7a7f7c2f8e974ce340ae93784283d51ed6b4c
scott <scottbakers@gmail.com>
2019-01-16 10:42:14 -0500

    app configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-sass",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
