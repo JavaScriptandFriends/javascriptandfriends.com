21fbb0c4fdf803f671eb8b45f53b5a79adeb87df
J.C. Hiatt <jc@jchiatt.com>
2019-03-08 22:33:55 -0600

    add CFP link

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,9 +23,10 @@ const Root = () => (
       <MainBanner>
         <Flex flexDirection="row" alignItems="center">
           <ButtonLink
-            href="#"
+            href="https://magnoliajs.typeform.com/to/K3uoNF"
             alignSelf="flex-start"
             mr={40}
+            target="_blank"
           >
             Submit a talk
           </ButtonLink>
@@ -68,13 +69,21 @@ const Root = () => (
         </Heading>
         <TextWrapper>
           <Text pt={10} pb={30}>
-            MagnoliaJS is Mississippi's <strong>first-ever developer conference</strong>. Our mission is to foster growth and opportunities for developers in the Southeast and be the launching point for greater things for the tech in Mississippi.
+            MagnoliaJS is Mississippi's{" "}
+            <strong>first-ever developer conference</strong>. Our mission is to
+            foster growth and opportunities for developers in the Southeast and
+            be the launching point for greater things for the tech in
+            Mississippi.
           </Text>
           <Text pt={10} pb={30}>
-            We are aiming to bring together around 200 web developers and friends to learn new skills and concepts and to network and develop new relationships.
+            We are aiming to bring together around 200 web developers and
+            friends to learn new skills and concepts and to network and develop
+            new relationships.
           </Text>
           <Text pb={40}>
-            Here's your chance to contribute to that mission! We're looking for the best companies to partner with us and help to make the first-annual MagnoliaJS conference a success.
+            Here's your chance to contribute to that mission! We're looking for
+            the best companies to partner with us and help to make the
+            first-annual MagnoliaJS conference a success.
           </Text>
         </TextWrapper>
         <Link
