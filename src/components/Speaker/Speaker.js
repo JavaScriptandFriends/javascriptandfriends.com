ad1de2cf15cbb19c15a6de79d089daa6ebdf0c4d
Levi Robertson <levi@lvrbrtsn.com>
2019-03-19 21:18:02 -0700

    implement speakers page, add all speakers names and images

--- src/components/Speaker/Speaker.js
+++ src/components/Speaker/Speaker.js
@@ -1,39 +1,54 @@
-import React, { Component } from "react";
-import styled from "styled-components";
+import React from "react";
 import { Flex } from "@rebass/grid";
 
-export default function Speaker(props) {
+export default function Speaker({ Image, name, talk }) {
   return (
     <div
       style={{
         width: 480,
         minWidth: 300,
-        maxHeight: 280,
-        margin: '1rem',
+        minHeight: 280,
+        margin: "1rem",
         backgroundColor: "#fff",
-        borderRadius: 12
+        borderRadius: 12,
+        display: "flex",
+        alignItems: "center"
       }}
     >
-      <Flex flexDirection='row' alignItems='center' style={{ padding: '1rem' }}>
-        <div style={{
-          margin: '0.5rem', width: '50%', textAlign: 'center'
-        }}>
-          <img
-            src="https://via.placeholder.com/150"
-            alt=""
-            style={{ borderRadius: "100%" }}
-          />
-          <h3>Laurie Voss</h3>
+      <Flex
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+        style={{ padding: "0.5rem", flexWrap: "wrap" }}
+      >
+        <div
+          style={{
+            margin: "0.5rem",
+            flex: 2
+          }}
+        >
+          <div style={{ width: "100%" }}>
+            <div
+              style={{
+                borderRadius: "100%",
+                overflow: "hidden",
+                width: "125px",
+                margin: "10px auto",
+                boxShadow: "0px 3px 15px rgba(0,0,0,0.2)"
+              }}
+            >
+              <Image />
+            </div>
+
+            <h3 style={{ textAlign: "center" }}>{name}</h3>
+          </div>
         </div>
 
-        <div style={{ margin: '0.5rem' }}>
-          <h3 style={{ fontSize: '1rem' }}>Talk title here</h3>
-          <p style={{fontSize: '1rem'}}>
-            talk description here. talk description here. talk description
-            here
-            </p>
+        <div style={{ width: "250px", margin: "0.5rem" }}>
+          <h3 style={{ fontSize: "1rem" }}>{talk.title}</h3>
+          <p style={{ fontSize: "1rem" }}>{talk.description}</p>
         </div>
       </Flex>
     </div>
-  )
+  );
 }
