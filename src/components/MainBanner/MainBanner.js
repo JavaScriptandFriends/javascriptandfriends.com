e09f7da0913fe89b692254778230517fcb5af699
J.C. Hiatt <jc@jchiatt.com>
2019-03-19 22:47:36 -0500

    add purchase link to homepage

--- src/components/MainBanner/MainBanner.js
+++ src/components/MainBanner/MainBanner.js
@@ -11,18 +11,8 @@ const Image = styled.img`
 `;
 
 function MainBanner({ children }) {
-  return (
-    <Flex
-      flexDirection={["column", "row-reverse"]}
-      my={[48, 0]}
-      alignItems="center"
-    >
-      <Box
-        width={[1, 1 / 2]}
-        alignSelf={["flex-start", "center"]}
-        justifyContent={["flex-start", "center"]}
-        px={30}
-      >
+  return <Flex flexDirection={["column", "row-reverse"]} my={[48, 0]} alignItems="center">
+      <Box width={[1, 1 / 2]} alignSelf={["flex-start", "center"]} justifyContent={["flex-start", "center"]} px={30}>
         <Flex justifyContent="center" flexDirection="column">
           <Heading size={1} color="light" my={0} fontSize={6}>
             MagnoliaJS Conference
@@ -30,20 +20,23 @@ function MainBanner({ children }) {
           <Text color="highlight" my={30}>
             Jackson, Mississippi | April 17 - April 18
           </Text>
+
+          <Flex justifyContent="center" flexDirection="column">
+            <Text color="white" my={30}>
+              Early Bird tickets on sale now!<br />
+              <a href="https://ti.to/magnoliajs/magnoliajs-2019" title="Click here to purchase tickets to MagnoliaJS" style={{ color: "yellow" }} target="_blank" rel="noopener noreferrer">
+                Purchase your ticket!
+              </a>
+            </Text>
+          </Flex>
+
           {children}
         </Flex>
-        <Flex justifyContent="center" flexDirection="column">
-          <Text color="highlight" my={30}>
-            Tickets on sale soon
-          </Text>
-        </Flex>
-        
       </Box>
       <Box width={[1, 1 / 2]}>
         <Image src={bannerImg} alt="" />
       </Box>
-    </Flex>
-  );
+    </Flex>;
 }
 
 export default MainBanner;
