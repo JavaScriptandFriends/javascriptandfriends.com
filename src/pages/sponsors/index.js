8edba56eb4a30c5642ed2f7e11aa31427aa99af1
baskarrao <rbaskar@selectsires.com>
2019-04-02 19:27:24 -0400

    Included Progress Kinvey Sponsor details
    Included Kinvey Sponsor Details. Also update logo height to 100 to avoid scaling issues.

--- src/pages/sponsors/index.js
+++ src/pages/sponsors/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Layout from "../../components/layout";
 import SponsorCard from "../../components/SponsorCard";
 import hmbLogo from "./HMB-green-finity.png";
+import progressLogo from "./Progress_Kinvey_Primary.png";
 import Alternate from "../../components/Alternate";
 export default class extends React.Component {
   render() {
@@ -14,7 +15,7 @@ export default class extends React.Component {
             title="HMB"
             href="https://hmbnet.com/"
             logoWidth={330}
-            logoHeight={206}
+            logoHeight={100}
             description={
               <>
                 <p>
@@ -35,6 +36,29 @@ export default class extends React.Component {
                   out of bed each morning. We come to work knowing we’re part of
                   building sensational technology that matters. This passion is
                   what drives us to succeed. It’s what we’re here to do.
+                </p>
+              </>
+            }
+          />
+          <SponsorCard
+            imageSrc={progressLogo}
+            title="Progress Kinvey"
+            href=" https://www.progress.com/kinvey"
+            logoWidth={330}
+            logoHeight={100}
+            description={
+              <>
+                <p>
+                Progress (NASDAQ: PRGS) offers the leading platform for developing and deploying strategic business applications. 
+                We enable customers and partners to deliver modern, high-impact digital experiences with a fraction of the effort, time and cost. 
+                Progress offers a high-productivity app development platform that increases productivity while maintaining developer control, 
+                powerful tools for building adaptive user experiences across any type of device or touchpoint,
+                machine learning that enables cognitive capabilities, a serverless cloud to deploy modern apps, 
+                plus business rules, web content management and leading data connectivity technology. 
+                </p>
+                <p>
+                Over 1,700 independent software vendors, 100,000 enterprise customers and two million developers rely on Progress to power their applications. 
+                Learn about Progress at www.progress.com or +1-800-477-6473. 
                 </p>
               </>
             }
