ed62a131a9a80a4a2f94f9afa9a7b137d6ceab12
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2019-05-27 23:19:16 -0400

    Quick updates for speakers page

--- src/components/Speaker/Speaker.js
+++ src/components/Speaker/Speaker.js
@@ -1,8 +1,25 @@
 import React, { useState } from "react";
 import { Flex } from "@rebass/grid";
-import { Button } from 'rebass'
 import Modal from "react-modal";
 import SpeakerModal from "./SpeakerModal";
+import styled from "styled-components";
+
+const NameLinks = styled.div`
+  @media (max-width: 560px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const Link = styled.button`
+  font-size: 0.75rem;
+  padding: 0;
+  border: 0;
+  background: none;
+  border-bottom: 1px solid #444;
+  display: flex;
+`;
 
 const truncate = content =>
   content
@@ -13,12 +30,53 @@ const truncate = content =>
 function Speaker({ Image, name, talk, twitter, linkedin, company, bio }) {
   const [isOpen, setOpen] = useState(false);
 
-  return <div onClick={x => setOpen(true)} style={{ width: 480, minWidth: 300, minHeight: 280, margin: "1rem", backgroundColor: "#fff", borderRadius: 12, display: "flex", alignItems: "center", boxShadow: "6px 9px 3px rgba(0 ,0,0,0.4)" }}>
-      <Modal isOpen={isOpen} style={{ overlay: { background: "rgba(51, 51, 51, 0.71)", overflow: "scroll" }, content: { background: "none", border: "none", margin: 0, padding: 0, overflow: "visible" } }} ariaHideApp={false}>
-        <SpeakerModal name={name} talk={talk} setOpen={setOpen} twitter={twitter} linkedin={linkedin} Image={Image} company={company} bio={bio}/>
+  return (
+    <div
+      onClick={x => setOpen(true)}
+      style={{
+        width: 480,
+        minWidth: 300,
+        minHeight: 280,
+        margin: "1rem",
+        backgroundColor: "#fff",
+        borderRadius: 12,
+        display: "flex",
+        alignItems: "center",
+        boxShadow: "6px 9px 3px rgba(0 ,0,0,0.4)"
+      }}
+    >
+      <Modal
+        isOpen={isOpen}
+        style={{
+          overlay: { background: "rgba(51, 51, 51, 0.71)", overflow: "scroll" },
+          content: {
+            background: "none",
+            border: "none",
+            margin: 0,
+            padding: 0,
+            overflow: "visible"
+          }
+        }}
+        ariaHideApp={false}
+      >
+        <SpeakerModal
+          name={name}
+          talk={talk}
+          setOpen={setOpen}
+          twitter={twitter}
+          linkedin={linkedin}
+          Image={Image}
+          company={company}
+          bio={bio}
+        />
       </Modal>
 
-      <Flex flexDirection="row" alignItems="center" justifyContent="center" style={{ padding: "0.5rem", flexWrap: "wrap" }}>
+      <Flex
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+        style={{ padding: "0.5rem", flexWrap: "wrap" }}
+      >
         <div style={{ margin: "0.5rem", flex: 2 }}>
           <div style={{ width: "100%" }}>
             <div
@@ -33,40 +91,47 @@ function Speaker({ Image, name, talk, twitter, linkedin, company, bio }) {
             >
               <Image />
             </div>
-
-            <h4 style={{ textAlign: "center" }}>{name}</h4>
-            <a
-              href={`https://twitter.com/${twitter}`}
-              title={`Go to ${name}'s Twitter`}
-              target="_blank"
-              rel="noopener noreferrer"
-              onclick="setOpen(false);"
-            >
-              Twitter
-            </a> &nbsp;
-            <a
-              href={`https://www.linkedin.com/in/${linkedin}`}
-              title={`Go to ${name}'s LinkedIn`}
-              target="_blank"
-              rel="noopener noreferrer"
-              onclick="setOpen(false);"
-            >
-              LinkedIn
-            </a>
+            <NameLinks>
+              <strong>{name}</strong>
+              <br />
+              <a
+                href={`https://twitter.com/${twitter}`}
+                title={`Go to ${name}'s Twitter`}
+                target="_blank"
+                rel="noopener noreferrer"
+                onclick="setOpen(false);"
+              >
+                Twitter
+              </a>{" "}
+              &nbsp;
+              <a
+                href={`https://www.linkedin.com/in/${linkedin}`}
+                title={`Go to ${name}'s LinkedIn`}
+                target="_blank"
+                rel="noopener noreferrer"
+                onclick="setOpen(false);"
+              >
+                LinkedIn
+              </a>
+            </NameLinks>
           </div>
         </div>
 
-        <div style={{ width: "250px", margin: "0.5rem" }}>
-          <h3 style={{ fontSize: "1rem" }}>{talk.title}</h3>
-
-          {company && 
-            <h4 style={{ fontSize: ".9rem" }}>Company: {company}</h4>
-          }
-          
-         
+        <div
+          style={{
+            width: "250px",
+            margin: "0.5rem",
+            flexDirection: "column"
+          }}
+        >
+          <h2 style={{ fontSize: "1rem" }}>{talk.title}</h2>
+          {company && (
+            <strong style={{ fontSize: ".9rem" }}>Company: {company}</strong>
+          )}
+          <Link>Learn more</Link>
         </div>
       </Flex>
     </div>
-  
+  );
 }
 export default Speaker;
