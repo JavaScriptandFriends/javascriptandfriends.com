ca7acf7d181079fc55b7ec6909d5bcd7423e4855
Calvin A. Allen <calvin.allen@outlook.com>
2019-05-29 11:24:24 -0400

    Adding manifest options for favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,9 @@ module.exports = {
         start_url: "/",
         background_color: "#663399",
         theme_color: "#663399",
-        display: "minimal-ui"
+        display: "minimal-ui",
+        display: "standalone",
+        icon: "src/images/logomark.png", 
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
