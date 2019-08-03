9bef59740c32034b7fcdd2153299f760b6964cdd
Calvin A. Allen <calvin.allen@outlook.com>
2019-08-03 19:09:16 -0400

    Changing site title to 2019

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   siteMetadata: {
-    title: "JavaScript and Friends Conference",
+    title: "2019 JavaScript and Friends Conference",
     description: "Columbus Ohio JavaScript Conference",
     author: "JavaScript and Friends Conference (volunteers)"
   },
