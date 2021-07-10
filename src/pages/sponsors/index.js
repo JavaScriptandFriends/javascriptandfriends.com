58e5ca636c88560b11bcdaae9d580160463c56b0
baskarmib <baskarrao.dandlamudi@outlook.com>
2021-07-10 01:05:12 -0400

    Updated code of conduct and sponsors
    Updated details

--- src/pages/sponsors/index.js
+++ src/pages/sponsors/index.js
@@ -6,6 +6,7 @@ import Heading from "../../components/Heading";
 import improvingLogo from "./improving.png";
 import authlogo from "./auth0.png";
 import scoutlogo from "./scoutlogo.png";
+import cloudinarylogo from "./cloudinary.png";
 import { Link } from "../../components/Button";
 export default class extends React.Component {
   render() {
@@ -52,6 +53,19 @@ Basically, we make your login box awesome.
               <>
                 <p>
                 Scout APM is leading-edge application performance monitoring designed to help Javascript developers quickly find and fix performance issues without having to deal with the headache or overhead of enterprise-platform feature bloat. With a developer-centric UI and tracing logic ties bottlenecks to source code, you can quickly pinpoint and resolve performance abnormalities like N+1 queries, slow database queries, memory bloat, and more. Scout's real-time alerting and weekly digest emails let you rest easy knowing Scout's on watch and resolving performance issues before your customers ever see them. See for yourself why developers worldwide call Scout their best friend ﻿and try our APM free for 14-days, no credit card needed! 
+                </p>                             
+              </>
+            }
+          />
+          <SponsorCard
+            imageSrc={cloudinarylogo}
+            title="Cloudinary"
+            href="https://cloudinary.rocks/eqn"
+            logoWidth={200}            
+            description={
+              <>
+                <p>
+                Cloudinary provides developers with powerful media API to upload, store, manage, optimize, transform and deliver images and videos. Developers utilize and easily integrate Cloudinary’s media experience platform via SDKs, widgets, add-ons into web and mobile apps built using all popular tech stacks. Cloudinary enables developers to deliver dynamic, high-performance, personalized media experiences on any device. <a href="https://discord.gg/MediaDevs" target="_blank" rel="noopener noreferrer">Join the community of media developers</a> 
                 </p>                             
               </>
             }
